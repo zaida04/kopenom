4596cfb3d8549c98ee1597d1d4fb3284a85ec850
Zaid (Nico) <nico.03727+github@gmail.com>
2023-02-24 12:43:18 -0500

    chore: cleanup

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ If you're anyone from nintendo's legal department, keep scrolling.
 
 ### Why does this exist?
 For practicing with Java, mostly with implementing stuff like LinkedLists. Done for my college class.
+
+### How to run?
+Open in your favorite IDE and run the tests in src/test/java.
--- src/main/java/org/kopenom/Main.java
+++ src/main/java/org/kopenom/Main.java
@@ -1,14 +1,5 @@
 package org.kopenom;
 
-//        LinkedListNode<Integer> a = new LinkedListNode<Integer>(2, null);
-//        a.append(4);
-//        a.append(5);
-//        a.append(9);
-//        a.append(3);
-//
-//        a.insert(134, 2);
-//        System.out.println(a.size());
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Showcase code can be found under the tests/ dir.");
